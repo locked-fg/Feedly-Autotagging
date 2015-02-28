ed20e9d8daea7a9287cbca42a03bd7f7bfb7942b
Franz <info@Locked.de>
2015-02-28 12:37:44 +0100

    imports

--- src/main/java/de/locked/feedly/Feedly.java
+++ src/main/java/de/locked/feedly/Feedly.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
